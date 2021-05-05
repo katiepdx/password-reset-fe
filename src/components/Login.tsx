cbbc2cadf34338c136ef1ea35acd5fb3c2e81318
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-05-05 10:25:57 -0700

    add login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { login } from '../utils/api-utils'
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
-  // check for token
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const history = useHistory()
+
+  const handleEmailChange = (e: any) => setEmail(e.target.value)
+  const handlePasswordChange = (e: any) => setPassword(e.target.value)
+
+  const handleLoginSubmit = async (e: any) => {
+    e.preventDefault()
+
+    const loginUser = await login({
+      email,
+      password
+    })
+
+    // check for login errors
+    if (loginUser.status === 500) return history.push('/login')
+
+    // redirect user on successful login
+    history.push('/')
+  }
 
   return (
     <div>
-      Login Page
+      <h1>Login</h1>
+      <form onSubmit={handleLoginSubmit}>
+        <label htmlFor="email"><div className='input-label'>Email:</div>
+          <input
+            type='email'
+            name="email"
+            placeholder='your@email.com'
+            value={email}
+            onChange={handleEmailChange} />
+        </label>
+
+        <label htmlFor="password"><div className='input-label'>Password:</div>
+          <input
+            type='password'
+            name="password"
+            placeholder='password'
+            value={password}
+            onChange={handlePasswordChange} />
+        </label>
+
+        <button>Login</button>
+      </form>
     </div>
   )
 }
--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -11,3 +11,17 @@ export const signUp = async (userInfo: object) => {
 
   return data
 }
+
+export const login = async (userInfo: object) => {
+  const res = await fetch('http://localhost:7890/api/v1/auth/login', {
+    method: 'POST',
+    body: JSON.stringify(userInfo),
+    headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+  })
+
+  if (!res.ok) alert('Invalid username/password. Please try again.')
+
+  const data = await res.json()
+
+  return data
+}
